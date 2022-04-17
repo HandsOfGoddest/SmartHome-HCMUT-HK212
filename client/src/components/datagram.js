132792110fe23e688b3548e9e0ee5f41b2ae3089
CuongFlodric <84664656+CuongFlodric@users.noreply.github.com>
2022-04-17 23:20:20 +0700

    Add files via upload

--- client/src/components/datagram.js
+++ client/src/components/datagram.js
@@ -10,37 +10,48 @@ async function getDatagram(dvId) {
         console.error(error);
     }
 }
-function AvgArray(arr,type) {
-    var sum = arr[0].data;
-    for (var i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum / arr.length;
-}
+
 
 function Datagram({ dvId,dvType,close }) {
     var dulieu = [];
-    const [plotType, setPlotType] = useState("hour");
+    const [plotType, setPlotType] = useState(10);
     const [data, setData] = useState([]);
     useEffect(() => {
         getDatagram(dvId).then(data => {
             setData(data);
         });
     }, []);
+
     if (data.length !== 0) {
-        data.map((dat) => {
-            if (dat._date_created) {
-                dulieu.push({
-                    'data': dat.data,
-                    'date': dat._date_created.split("T")[0].split("-")[2],
-                    'month': dat._date_created.split("T")[0].split("-")[1],
-                    'year': dat._date_created.split("T")[0].split("-")[0],
-                    'hour': dat._date_created.split("T")[1].split(":")[0],
-                })
+        if( data.length >= plotType){
+            for (var x = 0; x < plotType; x++) {
+                if (data[data.length - 1 -x]._date_created) {
+                    dulieu.push({
+                        'data': data[data.length - 1 -x].data,
+                        'date': data[data.length - 1 -x]._date_created.split("T")[0].split("-")[2],
+                        'month': data[data.length - 1 -x]._date_created.split("T")[0].split("-")[1],
+                        'year': data[data.length - 1 -x]._date_created.split("T")[0].split("-")[0],
+                        'hour': data[data.length - 1 -x]._date_created.split("T")[1].split(":")[0],
+                    })
+                }
             }
-        })
+            dulieu.reverse();
+        }
+        else{
+            data.map((dat) => {
+                if (dat._date_created) {
+                    dulieu.push({
+                        'data': dat.data,
+                        'date': dat._date_created.split("T")[0].split("-")[2],
+                        'month': dat._date_created.split("T")[0].split("-")[1],
+                        'year': dat._date_created.split("T")[0].split("-")[0],
+                        'hour': dat._date_created.split("T")[1].split(":")[0],
+                    })
+                }
+            })
+        }
     }
-    console.log(dulieu)
+    
     return (
         <React.Fragment >
             <div style={{width:"100%",display:"flex", justifyContent:"space-between"}}>
@@ -48,26 +59,19 @@ function Datagram({ dvId,dvType,close }) {
             </div>
             <div style={{textAlign:'center'}}>Biểu đồ {dvType}</div>
             <LineChart width={600} height={300} data={dulieu} style={{ fontSize: "15px" }} margin={{ top: 5, right: 10, bottom: 5, left: 0 }} >
-                <XAxis dataKey={plotType} />
+                <XAxis dataKey={"hour"} />
                 <YAxis />
                 <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
                 <Line type="monotone" dataKey="data" stroke="#8884d8" />
                 <Tooltip />
             </LineChart>
 
-            <div className="date-input"> 
-                <label htmlFor="date-start">From</label>
-                <input name='date-start' id='date-start' type="date" />
-                <label htmlFor="date-end">to</label>
-                <input name='date-end' id='date-end' type="date" />
-            </div>
             <div className="dv-input">  
-                <p>Đơn vị</p>
                 <select onChange={(e)=>setPlotType(e.target.value)} style={{alignSelf:'flex-end'}}>
-                    <option value="hour" selected>Giờ</option>
-                    <option value="date">Ngày</option>
-                    <option value="month">Tháng</option>
-                    <option value="year">Năm</option>
+                    <option value = {10} selected>10 record gần đây</option>
+                    <option value={20}>20 record gần đây</option>
+                    <option value={30}>30 record gần đây</option>
+                    <option value={50}>50 record gần đây</option>
                 </select> 
             </div>
         </React.Fragment>
